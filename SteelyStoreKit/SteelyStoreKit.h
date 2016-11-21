f403295d3ce1a6e808827d5f6047c24eef306a20
celanajaya <petermsteele@gmail.com>
2016-11-21 12:50:48 -0800

    Initial Commit

---
+++ SteelyStoreKit/SteelyStoreKit.h
@@ -0,0 +1,19 @@
+//
+//  SteelyStoreKit.h
+//  SteelyStoreKit
+//
+//  Created by Peter Steele on 11/21/16.
+//  Copyright © 2016 Peter Steele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SteelyStoreKit.
+FOUNDATION_EXPORT double SteelyStoreKitVersionNumber;
+
+//! Project version string for SteelyStoreKit.
+FOUNDATION_EXPORT const unsigned char SteelyStoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SteelyStoreKit/PublicHeader.h>
+
+
